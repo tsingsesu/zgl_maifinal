5adec48dd56fecca6b984eb9b8e8bf59632b9ee2
tsingsesu <tsingsesu@163.com>
2026-09-08 23:50:34 +0800

    完成主控制节点：如何判断到达标记点、如何判断已完成，目前主控制节点剩余速度指令发布

--- src/maicontrol_pkg/maicontrol_pkg/maincontrolnode.py
+++ src/maicontrol_pkg/maicontrol_pkg/maincontrolnode.py
@@ -23,22 +23,24 @@ class MaiMaincontrol(Node):
         self.start = self.get_parameter('start_point').value
         self.goal = self.get_parameter('goal_point').value
         self.hz = self.get_parameter('control_hz').value
-        self.v_cruise = self.get_parameter('curise_speed').value
+        self.v_cruise = self.get_parameter('cruise_speed').value
+        self.brake_dist = self.get_parameter('brake_dist').value
         self.tol = self.get_parameter('arrival_tolerance').value
 
         #通信 对应chassisnode中的相应部分 
         self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)#主控制向底盘发布建议速度
-        self.vel_pub = self.create_subscription(Twist, 'act_vel', 10)#底盘向主控返回实际速度
+        self.vel_sub = self.create_subscription(Twist, 'act_vel', self.vel_callback, 10)#底盘向主控返回实际速度
 
         self.x = WAYPOINTS[self.start][0]#先检索编号，然后取出字典右侧元组的第一个值当作x坐标
         self.y = WAYPOINTS[self.start][1]
 
         self.actual_vel = Twist()
         self.last_time = time.time()
-        self.current_target = NEXT[self.start]
+        self.current_target = NEXT[self.start]#字典，利用键值映射来表示“下一个”的概念
         self.steps = 0 #已走段数
-        self.finished = False
+        self.finished = False#完成记号，指示任务已完成用于终止速度发布，即小车停止运动
 
+        #以下日志只在初始化时打印，不会刷屏
         if self.mode == "patrol": 
             self.get_logger().info(f'任务一启动 从起点={self.start}  绕一圈巡航')
         else:
@@ -46,9 +48,8 @@ class MaiMaincontrol(Node):
 
         self.timer = self.create_timer(1.0/self.hz, self.control_callback)
 
-    def vel_callback(self, msg):
+    def vel_callback(self, msg):#负责接听的函数是不需要扔到定时执行器的，只需要订阅
         self.actual_vel = msg
-
 
     def control_callback(self):
         now = time.time()
@@ -57,17 +58,28 @@ class MaiMaincontrol(Node):
         self.y += self.actual_vel.linear.y *dt
         self.last_time = now 
         
-
-
-
-
-        delta_x = abs(abs(self.x)-abs(WAYPOINTS[NEXT[self.start + self.steps]][0]))
-        delta_y = abs(abs(self.y)-abs(WAYPOINTS[NEXT[self.start + self.steps]][1]))
+        tx, ty = WAYPOINTS[self.current_target]
+        delta_x = self.x - tx
+        delta_y = self.y - ty
         dist =  math.sqrt(delta_x**2 + delta_y**2)
 
         if dist < self.tol :
-            self.get_logger(f'已到达 {NEXT[self.start+self.steps]} 节点')        
+
+            arrived = self.current_target
             self.steps = self.steps +1
+            self.get_logger().info(f'已到达 {arrived} 节点')
+
+            #判断完成
+            if self.mode == 'point_to_point' and arrived == self.goal:
+                self.finished = True
+                self.get_logger().info(f'任务完成！已到达指定目的地，目的地编号: {self.goal} ，最终位置 ({self.x:.2f}, {self.y:.2f}),已经足够接近，可以认为到达')
+
+            elif self.mode == 'patrol' and self.steps == 8 :
+                self.finished == True
+                self.get_logger().info(f'任务完成！已环行一圈，最终位置 ({self.x:.2f}, {self.y:.2f},已经足够接近，可以认为到达')
+            else:
+                self.current_target = NEXT[arrived]       
+ 
 
 
 
